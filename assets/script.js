d2130acf325ca3611102fc7a788eb38dd0cf8add
Hannah Lee <hannahyvonne16@gmail.com>
2022-02-07 21:15:38 -0800

    bonus material

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ let yourName = "Hannah Lee"
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = 0 // Total
 
 const credit = document.querySelector('#credit')
 
@@ -28,21 +29,18 @@ gbPlusBtn.addEventListener('click', function () {
     console.log('Gingerbread + button was clicked!')
     gb++;
     gbQty.textContent = gb;
-    totalQty.textContent = gb;
 })
 
 ccPlusBtn.addEventListener('click', function () {
     console.log('Chocolate Chip + button was clicked!')
     cc++;
     ccQty.textContent = cc;
-    totalQty.textContent = cc;
 })
 
 sugarPlusBtn.addEventListener('click', function () {
     console.log('Sugar + button was clicked!')
     sugar++;
     sugarQty.textContent = sugar;
-    totalQty.textContent = sugar;
 })
 
 // Event listeners for clicks on the "-" button for cookies
@@ -50,22 +48,67 @@ sugarPlusBtn.addEventListener('click', function () {
 gbMinusBtn.addEventListener('click', function () {
     console.log('Gingerbread - button was clicked!')
     gb--;
+    if (gb < 1) gb = 0;
     gbQty.textContent = gb;
-    totalQty.textContent = gb;
 })
 
 ccMinusBtn.addEventListener('click', function () {
     console.log('Chocolate Chip - button was clicked!')
     cc--;
+    if (cc < 1) cc = 0;
     ccQty.textContent = cc;
-    totalQty.textContent = cc;
 })
 
 sugarMinusBtn.addEventListener('click', function () {
     console.log('Sugar - button was clicked!')
     sugar--;
+    if (sugar < 1) sugar = 0;
     sugarQty.textContent = sugar;
-    totalQty.textContent = sugar;
 })
 
+
 // Total quantity
+gbPlusBtn.addEventListener('click', function () {
+    console.log('Gingerbread Total + button was clicked!')
+    total++;
+    if (total < 1) total = 0;
+    totalQty.textContent = total;
+})
+
+ccPlusBtn.addEventListener('click', function () {
+    console.log('Chocolate Chip Total + button was clicked!')
+    total++;
+    if (total < 1) total = 0;
+    totalQty.textContent = total;
+})
+
+sugarPlusBtn.addEventListener('click', function () {
+    console.log('Sugar Total + button was clicked!')
+    total++;
+    if (total < 1) total = 0;
+    totalQty.textContent = total;
+})
+
+gbMinusBtn.addEventListener('click', function () {
+    console.log('Gingerbread Total - button was clicked!')
+    total--;
+    if (total < 1) total = 0;
+    totalQty.textContent = total;
+})
+
+ccMinusBtn.addEventListener('click', function () {
+    console.log('Chocolate Chip Total - button was clicked!')
+    total--;
+    if (total < 1) total = 0;
+    totalQty.textContent = total;
+})
+
+sugarMinusBtn.addEventListener('click', function () {
+    console.log('Sugar Total - button was clicked!')
+    total--;
+    if (total < 1) total = 0;
+    totalQty.textContent = total;    
+})
+
+// stop at 0
+
